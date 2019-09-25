8de460ca8a45cc2cfb05667909f824853abcfe4c
hepeng <6789@jkl>
2019-09-25 10:32:44 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'babel-polyfill'
 import router from './router/index.js'
 import store from './store/index.js'
 import axios from './public/axios.js'
