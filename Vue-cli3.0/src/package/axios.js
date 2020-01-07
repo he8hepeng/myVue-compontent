6a450df289dfc5b5342703f1f680bf2213ae295a
hepeng <6789@jkl>
2020-01-07 09:00:08 +0800

    提交代码
    增加 blob请求错误时 无法获取到json返回的message

--- Vue-cli3.0/src/package/axios.js
+++ Vue-cli3.0/src/package/axios.js
@@ -156,68 +156,36 @@ axios.interceptors.request.use(config => {
   return Promise.reject(error)
 })
 // 添加一个响应拦截器
-axios.interceptors.response.use(response => {
-  loadingInstance.close()
-  // 与后台沟通 204没有实体类 但前台需要实体类用以 promise回调 so 我们自己搞 按照各业务 可以删除或修改
-  if (response.status === 204) {
-    response.data = {
-      content: []
+axios.interceptors.response.use(
+  response => {
+    loadingInstance.close();
+    // 与后台沟通 204没有实体类 但前台需要实体类用以 promise回调 so 我们自己搞 按照各业务 可以删除或修改
+    if (response.status === 204) {
+      response.data = {
+        content: []
+      };
     }
-  }
-  return response
-}, err => {
-  modelIndex--
-  if (modelIndex === 0) {
-    loadingInstance.close()
-  }
-  let errMsg = ''
-  if (err && err.response.status) {
-    switch (err.response.status) {
-      case 401:
-        errMsg = '登录状态失效，请重新登录'
-        // localStorage.removeItem('tsToken')
-        // router.push('/login')
-        break
-      case 403:
-        errMsg = '拒绝访问'
-        break
-
-      case 408:
-        errMsg = '请求超时'
-        break
-
-      case 500:
-        errMsg = '服务器内部错误'
-        break
-
-      case 501:
-        errMsg = '服务未实现'
-        break
-
-      case 502:
-        errMsg = '网关错误'
-        break
-
-      case 503:
-        errMsg = '服务不可用'
-        break
-
-      case 504:
-        errMsg = '网关超时'
-        break
-
-      case 505:
-        errMsg = 'HTTP版本不受支持'
-        break
-
-      default:
-        errMsg = err.response.data.msg
-        break
+    return response;
+  },
+  error => {
+    modelIndex--;
+    if (modelIndex === 0) {
+      loadingInstance.close();
     }
-  } else {
-    errMsg = err
+    if (error.response !== undefined) {
+      if (
+        error.config.responseType === "blob" &&
+        error.response.data.type === "application/json"
+      ) {
+        let reader = new FileReader();
+        reader.readAsText(error.response.data, "utf-8");
+        reader.onload = e => {
+          Message.error(JSON.parse(reader.result));
+        };
+      } else {
+        Message.error(error.response.data.message);
+      }
+    }
+    return Promise.reject(error);
   }
-
-  Message.error(errMsg)
-  return Promise.reject(errMsg)
-})
+);
